6cdbeb4a3ea8d12002b3ac69e58090230a95216e
Obed Guzman Flores <230142@utxicotepec.edu.mx>
2024-09-25 11:59:29 -0600

    Practica 5
    Se agrego los tipos de inmutabilidad, mutabilidad, valores cuantitativos, cualitativos y desustructuracion de 2 o mas objetos

--- JS/Ejercicio_03.js
+++ JS/Ejercicio_03.js
@@ -102,6 +102,8 @@ Comprador_Clave:2005,
 Cantidad:5,
 Estatus: "Carrito de compra",
 Tipopago:"Efectivo" 
+
+
 }
 
 
@@ -119,10 +121,8 @@ Unidades, con un costo total de: ${costo_compra}
 if(costo_compra< Cliente_SaldoActual)
 console.log("El cliente tiene saldo suficiente");
 
-
 //Actualizar el valor de Objetos
 console.log("%c4.-Actualizacion de los valores de las propiedades de un objeto", style_console);
-
 console.log(`El objeto actualmente tiene los siguientes valores`)
 console.log(JSON.stringify(Producto2, null,2));
 Producto2.Disponibilidad="Si";
@@ -130,6 +130,11 @@ let nuevoTipoDisponibilidad= typeof(Producto2.Disponibilidad)
 console.log(Producto2);
 console.log(`El nuevo de dato de la disponibilidad es:${nuevoTipoDisponibilidad}`)
 
+//Convierte el Objeto a una cadena para evitar problemas con la referencia
+console.log('Por cuestiones de inflacion el costo del producto ha cambiado y debe ser actualizado')
+Producto=19000.00
+console.log(`Los nuevos valores del producto son: `)
+console.log(Producto2);
 
 
 
@@ -139,7 +144,123 @@ console.log(`El nuevo de dato de la disponibilidad es:${nuevoTipoDisponibilidad}
 
 
 
+//Agregar nuevas propiedades a un objeto  existente
+console.log("%c5.-Agregacion de propiedades de un objeto", style_console);
+console.log("Objeto antes de ser modificado:")
+console.table(Comprador);
+//Agregando propiedades
+Comprador['Direccion']= "Av. 05 de Mayo #25, interior 4A, Xicotepec de Juarez, Puebla,Mexico"
+Comprador['Tipo']= "Premium"
+Comprador['Estatus']= "Inactivo"
+Comprador['TotalCompras']= 50000.00
+console.log("Objetos despues de ser modificado")
+console.table(Comprador)
 
+
+//Eliminar propiedades de un objeto existente
+console.log("%c6.- Eliminacion de propiedades de un objeto (MUTACION)", style_console)
+console.log("Objeto antes de ser modificado")
+
+
+console.table(Pedido)
+//Eliminamos la propiedad de Tipó de pedido
+delete Pedido.Tipopago;
+console.log("Objeto despues de ser modifcado")
+console.table(Pedido)
+
+
+console.log("%c7.- Métodos para controlar la mutabilidad de los objetos, Congelacion (FREEZE)", style_console);
+
+//Si deseamos permitir que los objetos sean modificados ni en estructura, ni en valor, utilizaremos el metodo FREEZE (congelar)
+
+console.log('La estructura atual del objeto Comprador es: ' );
+console.table(Comprador)
+Object.freeze(Comprador)
+//Inteneramos agregar, eliminar o modificar los valores de sus propiedades
+Comprador.FechaUltimaCompra= "05/09/2024 10:15:25"
+delete Comprador.Tipo
+Comprador.Direccion = "Calle 16 de septiembre #102, col. Manantiales, Huachinango, Puebla,Mexico";
+console.log('Verificamos si se realizaron los cambios en el objeto Comprador: ')
+console.table(Comprador)
+
+
+console.log("%c8.- Metodos para controlar la mutailidad de lo Objetos, Sellado (SEAL",style_console);
+// Si embargo, en el caso que deseamos podeer podificar los valores de las propiedades del Objeto, pero no su estructura, usaremos SEAL
+Object.seal(Pedido)
+//Intentamos modificar su estructura
+Pedido['FechaPedido']="25/09/2024 11:05:13"
+delete Pedido['Cantidad']
+console.log('Vericamnos si realizaron los cambios en el objeto PEDIDO')
+console.table(Pedido);
+// Ahora intentamos modificar el valor de las propiedades
+Pedido.Cantidad= 12
+console.log('Verificamos si se realizaron los cambios en el objeto Pedido')
+console.table(Pedido)
+
+
+//Operaciones sobre objetos
+
+//Desustructuracion de 2 o mas objetos 
+console.log("%c9. - Desestructuracion de 2 o mas objetos", style_console);
+
+const {Precio:productoPrecio,SKU: ProductoSKU, Marca: ProductoMarca}=Producto;
+const {Correo:clientecorreo,PaisdeOrigen: clientePais, SaldoActual:clientesaldo, Tipo: clienteTipo}=Comprador;
+
+//Transformar valores cuantitativos en cualitativos
+if(productoPrecio>2000)
+    productoPrecio="Esta carisimo"
+else
+productoPrecio="Baratisimo"
+
+
+if(clientesaldo>0)
+    clientesaldo="A favor"
+else if(clientesaldo<0)
+clientesaldo="En contra"
+else
+clientesaldo="Sin deuda"
+
+// Transformar valores cualitativos en cuantitativos
+let clienteNivel;
+
+if(cliente=="Premium")
+    clienteNivel=1
+if(cliente=="Freemium")
+    clienteNivel=2
+
+if(cliente=="No identificado")
+    clienteNivel=3
+
+
+//Clasificamos al cliente por su País de origen
+
+if(clientePais=="Mexico")
+clientePais="Nacional"
+else
+clientePais="Extranjero"
+
+
+
+// OLE - Object Literal Ennhacemnet
+
+
+
+let datosClientePromociones ={clientecorreo, clientePais, clienteNivel, clientesaldo,ProductoMarca,ProductoPrecio
+}
+
+
+//El nuevo objeto que creamos seria un ejemplo de la informacion que enviaremos al area de Marketing para la difusion de promociones
+
+console.log("Los datos del cliente y sus habitos de compra son: ")
+console.table(datosClientePromociones)
+
+
+
+
+
+
+
+/*
 const producto= {
 
     nombre : "Tablet",
